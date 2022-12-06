e836054552e53796de37181eed30be80a4697629
rainesgan <7573395+rainesgan@user.noreply.gitee.com>
2022-12-06 22:17:46 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Header } from "./AppStyle";
+
+function App() {
+  return (
+    <div>
+      <Header> TEST </Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/AppStyle.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  padding: 5rem;
+  font-size: 5rem;
+  text-align: center;
+  background: #336699;
+  color: white;
+`;
