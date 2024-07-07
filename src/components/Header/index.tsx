8a7e20d374a1838e42f95a4a91c4d59c37343302
Raines <62054074+RainesG@users.noreply.github.com>
2024-07-07 16:10:39 +0800

    feat: init

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,9 @@
 import { Header } from "./styledComponents";
-
+import { Button } from "../button";
 const PageHeader = () => {
   return (
     <div>
+      <Button/>
       <Header> My Space </Header>
     </div>
   );
---
+++ src/components/button/index.tsx
@@ -0,0 +1,8 @@
+import styles from './button.module.scss';
+
+export const Button =()=>{
+    return <div>
+        <button>BUTTON</button>
+        <div className={styles.layout}></div>
+    </div>
+}
